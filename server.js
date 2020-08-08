2097bf43e6a926da8dee05392e38dc2fd2525fc9
Zach Winter <contact@zachwinter.com>
2020-08-08 16:54:11 -0700

    Server.

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+const fallback = require('express-history-api-fallback')
+const compression = require('compression')
+const root = path.resolve(__dirname, '../dist')
+const port = process.env.PORT || 8001
+const app = express()
+app.use(compression())
+app.use(express.static(root))
+app.use(fallback('index.html', { root }))
+app.listen(port, () => console.log('Listening on port ' + port))
