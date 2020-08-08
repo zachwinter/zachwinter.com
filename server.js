0dfb64452420f0c9c20c9e92300801d2b8b41314
Zach Winter <contact@zachwinter.com>
2020-08-08 16:57:46 -0700

    Server.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const fallback = require('express-history-api-fallback')
 const compression = require('compression')
-const root = path.resolve(__dirname, '../dist')
+const root = path.resolve(__dirname, './dist')
 const port = process.env.PORT || 8001
 const app = express()
 app.use(compression())
