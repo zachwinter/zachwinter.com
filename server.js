fd9c2b702cdfe5944f9569ccd3e8d74eeaa9df48
Zach Winter <contact@zachwinter.com>
2020-08-08 17:26:31 -0700

    Update.

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const compression = require('compression')
 const root = path.resolve(__dirname, './dist')
 const port = process.env.PORT || 8001
 const app = express()
+
 app.use(compression())
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
+
 app.listen(port, () => console.log('Listening on port ' + port))
